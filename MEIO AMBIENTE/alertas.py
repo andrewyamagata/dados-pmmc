efcf8fde9091ca5f966f98c153364ca5c032203b
Andrew Yamagata <andrewyamagata@gmail.com>
2025-02-27 09:33:10 -0300

    change csv

--- MEIO AMBIENTE/alertas.py
+++ MEIO AMBIENTE/alertas.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-arquivo = "alert_report.csv"
+arquivo = "alertas_estado_sp.csv"
 
 df = pd.read_csv(arquivo)
 
-df_mogi = df[df['state'].str.contains("São Paulo",na=False,case=False)]
+df_mogi = df[df['city'].str.contains("Mogi das Cruzes",na=False,case=False)]
 
-df_mogi.to_csv("alertas_estado_sp.csv", index=False, encoding="UTF-8")
+df_mogi.to_csv("alertas_mogi.csv", index=False, encoding="UTF-8")
 
 print("Arquivo Gerado com Sucesso")
